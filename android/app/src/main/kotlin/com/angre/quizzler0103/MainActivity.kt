38ffd3500dee6bb431b7178014d6e24c2dbf77a0
angre-kim <angre@daum.net>
2020-01-03 11:30:32 +0900

    처음

---
+++ android/app/src/main/kotlin/com/angre/quizzler0103/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.angre.quizzler0103
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
