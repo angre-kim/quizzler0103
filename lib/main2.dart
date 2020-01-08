4df77ee1a2331e972f50fe8ced69b11e04202eab
angre-kim <angre@daum.net>
2020-01-08 10:19:32 +0900

    처음

--- lib/main2.dart
+++ lib/main2.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(Quizzler());
@@ -25,28 +26,6 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-  ];
-
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
-  List<bool> answers = [
-    false, true, true
-  ];
-
-  int questionNumber = 0;
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -59,7 +38,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                '질문들',
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -76,24 +55,13 @@ class _QuizPageState extends State<QuizPage> {
               textColor: Colors.white,
               color: Colors.green,
               child: Text(
-                '참',
+                'true',
                 style: TextStyle(
                   color: Colors.white,
                   fontSize: 20.0,
                 ),
               ),
-              onPressed: () {
-
-                bool correctAnswer = answers[questionNumber];
-                if (correctAnswer == true) {
-                  print('정답');
-                }else {
-                  print('오답');
-                }
-                setState(() {
-                  questionNumber++;
-                });
-              },
+              onPressed: () {},
             ),
           ),
         ),
@@ -101,37 +69,23 @@ class _QuizPageState extends State<QuizPage> {
           child: Padding(
             padding: EdgeInsets.all(15.0),
             child: FlatButton(
-              color: Colors.red,
+              textColor: Colors.white,
+              color: Colors.green,
               child: Text(
-                '거짓',
+                'false',
                 style: TextStyle(
-                  fontSize: 20.0,
                   color: Colors.white,
+                  fontSize: 20.0,
                 ),
               ),
-              onPressed: () {
-                bool correctAnswer = answers[questionNumber];
-                if (correctAnswer == false) {
-                  print('정답');
-                }else {
-                  print('오답');
-                }
-
-
-
-                setState(() {
-                  questionNumber++;
-                });
-              },
+              onPressed: () {},
             ),
           ),
-        ),
-        Row(children: scoreKeeper),
+        )
       ],
     );
   }
 }
-
 /*
 question1: 'You can lead a cow down stairs but not up stairs.', false,
 question2: 'Approximately one quarter of human bones are in the feet.', true,
